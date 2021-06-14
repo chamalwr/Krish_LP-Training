5fa3f155467e56e7b953f653aebb1e0602248449
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-15 02:27:44 +0530

    Secured profile service with Access Token

---
+++ Microservices in Java/Secure-profile-Server[10]/auth-server-prod/src/test/java/dev/chamalwr/authserverprod/AuthServerProdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chamalwr.authserverprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Microservices in Java/Secure-profile-Server[10]/profile-service-oauth/src/main/java/dev/chamalwr/profileserviceoauth/ProfileServiceOauthApplication.java
@@ -0,0 +1,17 @@
+package dev.chamalwr.profileserviceoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EntityScan("dev.chamalwr.commons")
+@EnableResourceServer
+public class ProfileServiceOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileServiceOauthApplication.class, args);
+    }
+
+}
