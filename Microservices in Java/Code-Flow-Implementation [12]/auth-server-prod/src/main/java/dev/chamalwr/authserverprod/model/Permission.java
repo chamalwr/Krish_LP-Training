e424138a396617c5a3f054508c0cad52bb7022cb
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-15 13:55:07 +0530

    Added Code Flow Implementation

---
+++ Microservices in Java/Code-Flow-Implementation [12]/auth-server-prod/src/main/java/dev/chamalwr/authserverprod/model/Permission.java
@@ -0,0 +1,19 @@
+package dev.chamalwr.authserverprod.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Data
+@Entity
+@Table(name = "permission")
+public class Permission implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+}
+
