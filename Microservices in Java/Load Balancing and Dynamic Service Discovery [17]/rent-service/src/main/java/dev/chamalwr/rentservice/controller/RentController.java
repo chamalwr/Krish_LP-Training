cb338062f3830623d48525389f78723707f6eb16
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-16 14:40:36 +0530

    Added Load Balancing Implementation

---
+++ Microservices in Java/Load Balancing and Dynamic Service Discovery [17]/rent-service/src/main/java/dev/chamalwr/rentservice/controller/RentController.java
@@ -0,0 +1,41 @@
+package dev.chamalwr.rentservice.controller;
+
+import dev.chamalwr.rentmodel.model.rent.Rent;
+import dev.chamalwr.rentservice.model.Response;
+import dev.chamalwr.rentservice.model.SimpleResponse;
+import dev.chamalwr.rentservice.service.RentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/services/rents")
+public class RentController {
+
+    private RentService rentService;
+
+    @Autowired
+    public RentController(RentService rentService) {
+        this.rentService = rentService;
+    }
+
+    @PostMapping
+    public Rent save(@RequestBody Rent rent) {
+        return rentService.save(rent);
+    }
+
+    @GetMapping("/{id}")
+    public Response getRent(@PathVariable int id, @RequestParam(required = false) String type) {
+        if(type == null){
+            return new SimpleResponse(rentService.findById(id));
+        }else{
+            return rentService.findDetailResponse(id);
+        }
+    }
+
+    @GetMapping
+    public List<Rent> getAllRents() {
+        return rentService.findAll();
+    }
+}
