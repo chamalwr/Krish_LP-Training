4ed964bac1352aca06a157a4f81267d6b67e9a01
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-16 20:06:28 +0530

    [fix]Added deleted config for eureka client

--- Microservices in Java/Load Balancing and Dynamic Service Discovery [17]/rent-service/src/main/java/dev/chamalwr/rentservice/RentServiceApplication.java
+++ Microservices in Java/Load Balancing and Dynamic Service Discovery [17]/rent-service/src/main/java/dev/chamalwr/rentservice/RentServiceApplication.java
@@ -3,9 +3,11 @@ package dev.chamalwr.rentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = "dev.chamalwr.rentmodel.model.rent")
+@EnableEurekaClient
 public class RentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RentServiceApplication.class, args);
