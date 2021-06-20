95d40b82f4c96eeb51450419a2bdfbef1e69e1e3
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-06-20 22:05:11 +0530

    Added main modules and configure routing

--- Angular/pet-clinic/src/app/app-routing.module.ts
+++ Angular/pet-clinic/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'pet', loadChildren: () => import('./pet/pet.module').then(m => m.PetModule) }, { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
